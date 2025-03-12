e6b08ad41214c5c6839c91def72a848d2a1c8c11
Haolin07 <haolin.24@intl.zju.edu.cn>
2025-03-12 13:41:57 +0800

    1

---
+++ triangle.py
@@ -0,0 +1,6 @@
+#set the first number
+a=1
+#add the extra number
+for i in range(2,12):
+    print(a)
+    a=a+i
---
+++ variables.py
@@ -0,0 +1,12 @@
+a=15
+b=75
+c=a+b
+d=90
+e=5
+f=d+e
+if c>f:
+    print("car")
+elif c<f:
+    print("bus")
+else:
+    print("equal")
