e9439b0a34d2286d3cef34130ddee64cd9a264cb
Haolin07 <haolin.24@intl.zju.edu.cn>
2025-03-14 11:20:01 +0800

    1

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,7 +1,7 @@
 #input the height and weight and stored as two variables
 a=float (input("please input your height in meter" ))
 b=float (input("please input yourn weight in kg" ))
-#set a new variable to store BMI
+#set a new variable to store and calculate BMI 
 c=b/a**2
 #output the result
 print("your BMI is" , c)
